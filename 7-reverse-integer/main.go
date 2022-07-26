07ea3423b92d7891c499a6284414ebb72c41b928
Panmax <jiapan.china@gmail.com>
2022-07-26 14:19:24 +0800

    reverse integer

---
+++ 7-reverse-integer/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func reverseInt(num int) int {
+	result := 0
+	for num != 0 {
+		last := num % 10
+		num = num / 10
+		result = result*10 + last
+		if result > math.MaxInt32 || result < math.MinInt32 {
+			return 0
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(reverseInt(0))
+	fmt.Println(reverseInt(1234))
+	fmt.Println(reverseInt(-321))
+	fmt.Println(reverseInt(-120))
+	fmt.Println(reverseInt(120))
+}
