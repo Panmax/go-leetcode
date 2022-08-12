c70d29252f8af5d12e6a286ffa6579a2524d49a1
Panmax <jiapan.china@gmail.com>
2022-08-12 13:25:54 +0800

    20220812

---
+++ 94-binary-tree-inorder-traversal/main.go
@@ -0,0 +1,22 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	res := make([]int, 0)
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return res
+}
