bf8300944d857d31d748d80e901e08137611dd91
jiapan <jiapan.china@gmail.com>
2022-08-17 07:49:05 +0800

    20220817

---
+++ 94-binary-tree-inorder-traversal/review_20220817.go
@@ -0,0 +1,17 @@
+package main
+
+func inorderTraversal_20220816(root *TreeNode) []int {
+	var ans []int
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		// 这里一定要注意，每次都忘判断
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		ans = append(ans, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return ans
+}
