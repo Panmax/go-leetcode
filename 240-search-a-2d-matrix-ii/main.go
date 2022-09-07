94885231f76e6bf9b8e1bc3b0b43ad58c1f5e72f
Panmax <jiapan.china@gmail.com>
2022-09-07 10:11:57 +0800

    20220907

--- 240-search-a-2d-matrix-ii/main.go
+++ 240-search-a-2d-matrix-ii/main.go
@@ -1,7 +1,18 @@
 package main
 
 func searchMatrix(matrix [][]int, target int) bool {
-
+	m, n := len(matrix), len(matrix[0])
+	x, y := 0, n-1
+	for x < m && y >= 0 {
+		if matrix[x][y] == target {
+			return true
+		} else if matrix[x][y] > target {
+			y--
+		} else {
+			x++
+		}
+	}
+	return false
 }
 
 func searchMatrixLoop(matrix [][]int, target int) bool {
