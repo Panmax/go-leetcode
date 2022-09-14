19d50b67ca8c7a81a8b6a25c5b080fcecaa0ca46
Panmax <jiapan.china@gmail.com>
2022-09-14 09:44:00 +0800

    20220913

--- 229-majority-element-ii/main.go
+++ 229-majority-element-ii/main.go
@@ -1,10 +1,12 @@
 package main
 
-func majorityElement_20220914(nums []int) []int {
+// 超过 1/2 的最多有一个，超过 1/3 的最多有两个
+func majorityElement(nums []int) []int {
 	res := make([]int, 0)
 	if len(nums) == 0 {
 		return res
 	}
+
 	num1, num2 := nums[0], nums[0]
 	counter1, counter2 := 0, 0
 	for _, num := range nums {
@@ -18,22 +20,25 @@ func majorityElement_20220914(nums []int) []int {
 		}
 		if counter1 == 0 {
 			num1 = num
+			// 恢复到1
 			counter1 = 1
 			continue
 		}
 		if counter2 == 0 {
 			num2 = num
+			// 恢复到1
 			counter2 = 1
 			continue
 		}
 		counter1--
 		counter2--
 	}
+
 	counter1, counter2 = 0, 0
 	for _, num := range nums {
 		if num1 == num {
 			counter1++
-		} else if num2 == num {
+		} else if num2 == num { // else if
 			counter2++
 		}
 	}
@@ -44,5 +49,20 @@ func majorityElement_20220914(nums []int) []int {
 		res = append(res, num2)
 	}
 
+	return res
+}
+
+// 放到字典里，把计数大于 t 的找出来
+func majorityElementWithMap(nums []int) []int {
+	res := make([]int, 0)
+	m := make(map[int]int)
+	for _, num := range nums {
+		m[num]++
+	}
+	for num, c := range m {
+		if c > len(nums)/3 {
+			res = append(res, num)
+		}
+	}
 	return res
 }
