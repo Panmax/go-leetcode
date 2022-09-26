b867b084af3191106ea4b74410ae83d485a3d4d1
Panmax <jiapan.china@gmail.com>
2022-09-26 12:37:06 +0800

    20220926

---
+++ 402-remove-k-digits/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "strings"
+
+func removeKdigits(num string, k int) string {
+	stack := []byte{}
+	for i := range num {
+		// for
+		for k > 0 && len(stack) > 0 && num[i] < stack[len(stack)-1] {
+			k--
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, num[i])
+	}
+	stack = stack[:len(stack)-k]
+
+	res := strings.TrimLeft(string(stack), "0")
+	if res == "" {
+		res = "0"
+	}
+	return res
+}
