46a378cbfb3981f3842fde13a826081261beb037
jiapan <jiapan.china@gmail.com>
2022-10-06 16:21:22 +0800

    202201005

---
+++ 164-maximum-gap/review_20221005.go
@@ -0,0 +1,41 @@
+package main
+
+import "math"
+
+func maximumGap_20221005(nums []int) int {
+	if len(nums) < 2 {
+		return 0
+	}
+	min := math.MaxInt32
+	max := math.MinInt32
+	for _, num := range nums {
+		if num < min {
+			min = num
+		}
+		if num > max {
+			max = num
+		}
+	}
+	bucketSize := Max(1, (max-min)/(len(nums)-1))
+	buckets := make([]*Bucket, (max-min)/bucketSize+1)
+	for _, num := range nums {
+		loc := (num - min) / bucketSize
+		if buckets[loc] == nil {
+			buckets[loc] = NewBucket()
+		}
+		buckets[loc].Min = Min(buckets[loc].Min, num)
+		buckets[loc].Max = Max(buckets[loc].Max, num)
+	}
+	preMax := math.MaxInt32
+	maxGap := 0
+	for _, bucket := range buckets {
+		if bucket != nil && preMax != math.MaxInt32 {
+			maxGap = Max(maxGap, bucket.Min-preMax)
+		}
+		if bucket != nil {
+			preMax = bucket.Max
+			maxGap = Max(maxGap, bucket.Max-bucket.Min)
+		}
+	}
+	return maxGap
+}
