803abfc8362fb21430776dd3d614c29fffd09083
Panmax <jiapan.china@gmail.com>
2022-10-10 14:37:23 +0800

    20221010

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/main.go
@@ -0,0 +1,29 @@
+package main
+
+func kthSmallest(matrix [][]int, k int) int {
+	n := len(matrix)
+	left, right := matrix[0][0], matrix[n-1][n-1]
+	for left < right {
+		mid := left + (right-left)/2
+		if isInLeft(matrix, mid, k, n) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func isInLeft(matrix [][]int, mid int, k int, n int) bool {
+	i, j := n-1, 0
+	count := 0
+	for i >= 0 && j < n {
+		if matrix[i][j] <= mid {
+			count += i + 1
+			j++
+		} else {
+			i--
+		}
+	}
+	return count >= k
+}
