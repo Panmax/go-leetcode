e6022bf67fafc590795dfa522d403dd26816d269
Panmax <jiapan.china@gmail.com>
2022-10-18 16:15:17 +0800

    20221018

---
+++ 107-binary-tree-level-order-traversal-ii/main.go
@@ -0,0 +1,38 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	var res [][]int
+	if root == nil {
+		return nil
+	}
+	var queue []*TreeNode
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		var level []int
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			level = append(level, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		res = append(res, level)
+	}
+
+	for i := 0; i < len(res)/2; i++ {
+		res[i], res[len(res)-i-1] = res[len(res)-i-1], res[i]
+	}
+
+	return res
+}
