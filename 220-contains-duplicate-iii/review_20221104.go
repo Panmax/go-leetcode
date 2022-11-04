902358a7d230788cf61bc344b17ac0e4ad23b5a5
Panmax <jiapan.china@gmail.com>
2022-11-04 12:20:29 +0800

    20221104

---
+++ 220-contains-duplicate-iii/review_20221104.go
@@ -0,0 +1,23 @@
+package main
+
+func containsNearbyAlmostDuplicate_20221104(nums []int, indexDiff int, valueDiff int) bool {
+	bucketSize := valueDiff + 1
+	m := make(map[int]int)
+	for i, num := range nums {
+		loc := getBucketId(num, bucketSize)
+		if _, exist := m[loc]; exist {
+			return true
+		}
+		if x, exist := m[loc-1]; exist && abs(x-num) <= valueDiff {
+			return true
+		}
+		if x, exist := m[loc+1]; exist && abs(x-num) <= valueDiff {
+			return true
+		}
+		m[loc] = num
+		if i >= indexDiff {
+			delete(m, getBucketId(nums[i-indexDiff], bucketSize))
+		}
+	}
+	return false
+}
---
+++ 450-delete-node-in-a-bst/review_20221104.go
@@ -0,0 +1,26 @@
+package main
+
+func deleteNode_20221104(root *TreeNode, key int) *TreeNode {
+	switch {
+	case root == nil:
+		return nil
+	case root.Val > key:
+		root.Left = deleteNode(root.Left, key)
+	case root.Val < key:
+		root.Right = deleteNode(root.Right, key)
+	case root.Right == nil || root.Left == nil:
+		if root.Left != nil {
+			return root.Left
+		}
+		return root.Right
+	default:
+		successor := root.Right
+		for successor.Left != nil {
+			successor = successor.Left
+		}
+		successor.Right = deleteNode(root.Right, successor.Val)
+		successor.Left = root.Left
+		return successor
+	}
+	return root
+}
