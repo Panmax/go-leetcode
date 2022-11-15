c22b63b7ad0a82f93fa8fe501fe2de3301c8c336
Panmax <jiapan.china@gmail.com>
2022-11-15 10:30:08 +0800

    20221115

---
+++ 33-search-in-rotated-sorted-array/review_20221115.go
@@ -0,0 +1,31 @@
+package main
+
+func search_20221115(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		// <=
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
