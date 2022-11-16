8f6062ef5122b20652da6c82cd7b114a23b9b6b6
Panmax <jiapan.china@gmail.com>
2022-11-16 12:10:48 +0800

    20221116

---
+++ 516-longest-palindromic-subsequence/main.go
@@ -0,0 +1,27 @@
+package main
+
+func longestPalindromeSubseq(s string) int {
+	n := len(s)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+	for i := n - 1; i >= 0; i-- {
+		dp[i][i] = 1
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][n-1]
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
