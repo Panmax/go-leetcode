050776ab2073fa72d585aeec0e37e891fab69526
Panmax <jiapan.china@gmail.com>
2022-12-06 12:49:34 +0800

    20221206

---
+++ 279-perfect-squares/main.go
@@ -0,0 +1,20 @@
+package main
+
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		dp[i] = i
+		// i-j*j >= 0
+		for j := 1; i-j*j >= 0; j++ {
+			dp[i] = min(dp[i], dp[i-j*j]+1)
+		}
+	}
+	return dp[n]
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
---
+++ 279-perfect-squares/review_20221206.go
@@ -0,0 +1,12 @@
+package main
+
+func numSquares_20221206(n int) int {
+	dp := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		dp[i] = i
+		for j := 1; i-j*j >= 0; j++ {
+			dp[i] = min(dp[i], dp[i-j*j]+1)
+		}
+	}
+	return dp[n]
+}
