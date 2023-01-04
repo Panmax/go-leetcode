ba8b0efefe117b9f390b2ac93ac76ebbbd6e6450
Panmax <jiapan.china@gmail.com>
2023-01-04 11:45:53 +0800

    20230104

---
+++ 142-linked-list-cycle-ii/review_20230104.go
@@ -0,0 +1,18 @@
+package main
+
+func detectCycle_20230104(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for slow != nil && fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			ptr := head
+			for ptr != slow {
+				ptr = ptr.Next
+				slow = slow.Next
+			}
+			return ptr
+		}
+	}
+	return nil
+}
