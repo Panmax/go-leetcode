33271331c6cc905405e08bad36670307475b4d15
Panmax <jiapan.china@gmail.com>
2023-01-05 13:05:24 +0800

    20230105

---
+++ 334-increasing-triplet-subsequence/review_20230105.go
@@ -0,0 +1,25 @@
+package main
+
+func increasingTriplet_20230105(nums []int) bool {
+	n := len(nums)
+	if n < 3 {
+		return false
+	}
+	leftMin := make([]int, n)
+	leftMin[0] = nums[0]
+	for i := 1; i < n; i++ {
+		leftMin[i] = min(leftMin[i-1], nums[i])
+	}
+
+	rightMax := make([]int, n)
+	rightMax[n-1] = nums[n-1]
+	for i := n - 2; i >= 0; i-- {
+		rightMax[i] = max(rightMax[i+1], nums[i])
+	}
+	for i := 1; i < n-1; i++ {
+		if leftMin[i] < nums[i] && rightMax[i] > nums[i] {
+			return true
+		}
+	}
+	return false
+}
