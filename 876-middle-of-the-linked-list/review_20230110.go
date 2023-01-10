57f24f31df890909c542a6028464982884e2590b
Panmax <jiapan.china@gmail.com>
2023-01-10 13:47:29 +0800

    20230110

---
+++ 876-middle-of-the-linked-list/review_20230110.go
@@ -0,0 +1,10 @@
+package main
+
+func middleNode_20220110(head *ListNode) *ListNode {
+    slow, fast := head, head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    return slow
+}
