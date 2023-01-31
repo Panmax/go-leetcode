f3b61dbeb4765dc3cb65cfbb7cd9fb07249cfd08
Panmax <jiapan.china@gmail.com>
2023-01-31 13:37:31 +0800

    20230131

--- 48-rotate-image/main.go
+++ 48-rotate-image/main.go
@@ -4,7 +4,7 @@ func rotate(matrix [][]int) {
 	n := len(matrix)
 	for i := 0; i < n/2; i++ {
 		for j := i; j < n-1-i; j++ {
-			matrix[i][j], matrix[n-1-j][i], matrix[n-1-i][n-1-j], matrix[i][n-1-j] =
+			matrix[i][j], matrix[n-1-j][i], matrix[n-1-i][n-1-j], matrix[j][n-1-i] =
 				matrix[n-1-j][i], matrix[n-1-i][n-1-j], matrix[j][n-1-i], matrix[i][j]
 		}
 	}
