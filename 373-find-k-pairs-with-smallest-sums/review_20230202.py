ea15ec84c50051813572fb29dc028d28486fb92a
Panmax <jiapan.china@gmail.com>
2023-02-02 14:33:28 +0800

    20230202

---
+++ 373-find-k-pairs-with-smallest-sums/review_20230202.py
@@ -0,0 +1,15 @@
+import heapq
+
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        m = len(nums1)
+        n = len(nums2)
+        pq = [(nums1[i]+nums2[0], i, 0) for i in range(min(k, m))]
+        res = []
+        while pq and len(res) < k:
+            _, i, j = heapq.heappop(pq)
+            res.append([nums1[i], nums2[j]])
+            if j+1 < n:
+                heapq.heappush(pq, (nums1[i]+nums2[j+1], i, j+1))
+        return res
