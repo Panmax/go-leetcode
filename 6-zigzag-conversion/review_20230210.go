b8bdb6cb3b4b7a1074075af1c8055302708a7b8a
Panmax <jiapan.china@gmail.com>
2023-02-10 11:53:44 +0800

    20230210

---
+++ 6-zigzag-conversion/review_20230210.go
@@ -0,0 +1,22 @@
+package mian
+
+func convert_20230210(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	rows := make([]string, numRows)
+	i := 0
+	flag := -1
+	for _, c := range s {
+		rows[i] += string(c)
+		if i == 0 || i == numRows-1 {
+			flag = -flag
+		}
+		i += flag
+	}
+	var res string
+	for _, s := range rows {
+		res += s
+	}
+	return res
+}
