61845e3e270e63b5df095512f8b41d1f53f01dce
jiapan <jiapan.china@gmail.com>
2023-02-27 15:19:55 +0800

    20230227

---
+++ 141-linked-list-cycle/main.go
@@ -0,0 +1,22 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow := head
+	fast := head.Next
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
