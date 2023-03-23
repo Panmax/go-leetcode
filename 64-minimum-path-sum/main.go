687011c53648ecb02ae1ef5d74e96e96ef5312eb
jiapan <jiapan.china@gmail.com>
2023-03-23 14:08:57 +0800

    20230323

---
+++ 64-minimum-path-sum/main.go
@@ -0,0 +1,25 @@
+package main
+
+func minPathSum(grid [][]int) int {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if i == 0 && j == 0 {
+				continue
+			} else if i == 0 {
+				grid[i][j] = grid[i][j-1] + grid[i][j]
+			} else if j == 0 {
+				grid[i][j] = grid[i-1][j] + grid[i][j]
+			} else {
+				grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j]
+			}
+		}
+	}
+	return grid[len(grid)-1][len(grid[0])-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
