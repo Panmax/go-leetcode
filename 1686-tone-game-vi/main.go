bd0e251c942317d35029ad44501a485f1b317b15
jiapan <jiapan.china@gmail.com>
2023-03-28 14:34:32 +0800

    20230328

--- 1686-tone-game-vi/main.go
+++ 1686-tone-game-vi/main.go
@@ -10,17 +10,20 @@ type Value struct {
 func stoneGameVI(aliceValues []int, bobValues []int) int {
 	n := len(aliceValues)
 	var values []*Value
+	// 两个人认为的收益加起来
 	for i := 0; i < n; i++ {
 		values = append(values, &Value{
 			index: i,
 			value: aliceValues[i] + bobValues[i],
 		})
 	}
+	// 按降序排序
 	sort.Slice(values, func(i, j int) bool {
 		return values[i].value > values[j].value
 	})
 	alice := 0
 	bob := 0
+	// i%2==0时，取 alice 认为最大收益的
 	for i, value := range values {
 		if i%2 == 0 {
 			alice += aliceValues[value.index]
