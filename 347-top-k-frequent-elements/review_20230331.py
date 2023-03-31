b1bafa9c6c26e7d1a03359aa6f4d3cda5d7dec22
jiapan <jiapan.china@gmail.com>
2023-03-31 13:27:57 +0800

    20230331

---
+++ 347-top-k-frequent-elements/review_20230331.py
@@ -0,0 +1,22 @@
+
+
+import heapq
+
+
+def topKFrequent_20230331(nums, k):
+    m = {}
+    for num in nums:
+        m[num] = m.get(num, 0) + 1
+
+    pq = []
+    for num, count in m.items():
+        heapq.heappush(pq, (count, num))
+        if len(pq) > k:
+            heapq.heappop(pq)
+
+    res = []
+    while pq:
+        _, num = heapq.heappop(pq)
+        res.append(num)
+
+    return res
