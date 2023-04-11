15b3fe53826076880353e87dea50119089a17871
jiapan <jiapan.china@gmail.com>
2023-04-11 10:35:33 +0800

    20230411

---
+++ 141-linked-list-cycle/review_20230411.go
@@ -0,0 +1,16 @@
+package main
+
+func hasCycle_20230411(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
