6095fad66fd1abaedd17164d1f61b7c84ae4efb2
jiapan <jiapan.china@gmail.com>
2023-04-12 10:36:21 +0800

    20230412

---
+++ 1686-tone-game-vi/review_20230412.go
@@ -0,0 +1,31 @@
+package main
+
+import "sort"
+
+func stoneGameVI_20230412(aliceValues []int, bobValues []int) int {
+	var values []*Value
+	for i := 0; i < len(aliceValues); i++ {
+		values = append(values, &Value{
+			index: i,
+			value: aliceValues[i] + bobValues[i],
+		})
+	}
+	sort.Slice(values, func(i, j int) bool {
+		return values[i].value > values[j].value
+	})
+	var alice, bob int
+	for i := 0; i < len(values); i++ {
+		if i%2 == 0 {
+			alice += aliceValues[values[i].index]
+		} else {
+			bob += bobValues[values[i].index]
+		}
+	}
+	if alice > bob {
+		return 1
+	} else if alice < bob {
+		return -1
+	} else {
+		return 0
+	}
+}
