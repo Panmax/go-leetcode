cda78216b534aca65697766987b1dcc72d11ef20
jiapan <jiapan.china@gmail.com>
2023-04-27 11:56:26 +0800

    20230427

---
+++ 53-maximum-subarray/review_20230427.go
@@ -0,0 +1,14 @@
+package main
+
+func maxSubArray_20230427(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > 0 {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
