c2af5ead6541eb029e97d3677742cf555df2feff
jiapan <jiapan.china@gmail.com>
2023-05-17 11:22:10 +0800

    20230517

---
+++ 226-invert-binary-tree/review_20230517.go
@@ -0,0 +1,19 @@
+package main
+
+func invertTree_20230517(root *TreeNode) *TreeNode {
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		if node.Left != nil {
+			dfs(node.Left)
+		}
+		if node.Right != nil {
+			dfs(node.Right)
+		}
+		node.Left, node.Right = node.Right, node.Left
+	}
+	dfs(root)
+	return root
+}
