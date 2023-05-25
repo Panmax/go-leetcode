c5e52b1d5cd44c903e524c87a049a22fd775a90f
jiapan <jiapan.china@gmail.com>
2023-05-25 11:48:24 +0800

    20230525

--- 28-find-the-index-of-the-first-occurrence-in-a-string/main.go
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/main.go
@@ -1,6 +1,21 @@
 package main
 
-// https://www.youtube.com/watch?v=af1oqpnH1vA
 func strStr(haystack string, needle string) int {
-
+	if len(needle) == 0 {
+		return 0
+	}
+	for i := 0; i < len(haystack); i++ {
+		if i+len(needle) > len(haystack) {
+			break
+		}
+		for j := 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+			if j == len(needle)-1 {
+				return i
+			}
+		}
+	}
+	return -1
 }
