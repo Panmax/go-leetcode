726dd96ae9dca0cb3b6049650370d621c486f4da
Panmax <jiapan.china@gmail.com>
2023-06-18 15:56:45 +0800

    20230618

---
+++ 26-remove-duplicates-from-sorted-array/review_20230618.go
@@ -0,0 +1,16 @@
+package main
+
+func removeDuplicates_20230618(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < n; fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/review_20230618.go
@@ -0,0 +1,22 @@
+package main
+
+func strStr_20230618(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	for i := 0; i < len(haystack); i++ {
+		if i+len(needle) > len(haystack) {
+			break
+		}
+		for j := 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+			if j == len(needle)-1 {
+				return i
+			}
+		}
+	}
+
+	return -1
+}
