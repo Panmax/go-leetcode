412ba9f2123f4104b5aab665db17c62d36d08c8b
jiapan <jiapan.china@gmail.com>
2023-06-26 09:44:11 +0800

    20230626

---
+++ 79-word-search/review_20230626.go
@@ -0,0 +1,42 @@
+package main
+
+func exist_20230626(board [][]byte, word string) bool {
+	w, h := len(board), len(board[0])
+	visited := make([][]bool, w)
+	for i := range visited {
+		visited[i] = make([]bool, h)
+	}
+	var check func(i, j, k int) bool
+	check = func(i, j, k int) bool {
+		if board[i][j] != word[k] {
+			return false
+		}
+		if k == len(word)-1 {
+			return true
+		}
+		visited[i][j] = true
+		defer func() {
+			visited[i][j] = false
+		}()
+		for _, dir := range dirs {
+			newI := i + dir.x
+			newJ := j + dir.y
+			if newI >= 0 && newI < w && newJ >= 0 && newJ < h && !visited[newI][newJ] {
+				if check(newI, newJ, k+1) {
+					return true
+				}
+			}
+		}
+
+		return false
+	}
+
+	for i, row := range board {
+		for j := range row {
+			if check(i, j, 0) {
+				return true
+			}
+		}
+	}
+	return false
+}
