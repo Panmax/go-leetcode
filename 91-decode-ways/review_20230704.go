70b607b453a2abbc30be492f1f8ed628f7f9196c
jiapan <jiapan.china@gmail.com>
2023-07-04 09:42:05 +0800

    20230704

---
+++ 91-decode-ways/review_20230704.go
@@ -0,0 +1,16 @@
+package main
+
+func numDecodings_20230704(s string) int {
+	dp := make([]int, len(s)+1)
+	dp[0] = 1
+	for i := 1; i < len(s)+1; i++ {
+		if s[i-1] != '0' {
+			dp[i] = dp[i-1]
+		}
+		if i > 1 && s[i-2] != '0' && ((s[i-2]-'0')*10+(s[i-1]-'0')) <= 26 {
+			dp[i] += dp[i-2]
+		}
+	}
+
+	return dp[len(s)]
+}
