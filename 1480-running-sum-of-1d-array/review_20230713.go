6e69ec106e5a6a9e31aee8f25f0a2c64b5204b56
jiapan <jiapan.china@gmail.com>
2023-07-13 13:55:22 +0800

    20230713

---
+++ 1480-running-sum-of-1d-array/review_20230713.go
@@ -0,0 +1,11 @@
+package main
+
+func runningSum_20230713(nums []int) []int {
+	res := make([]int, len(nums))
+	res[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		res[i] = nums[i] + res[i-1]
+	}
+
+	return res
+}
