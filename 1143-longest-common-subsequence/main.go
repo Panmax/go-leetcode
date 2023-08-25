c84c72fce2f65827de535a464e725e1544aa1709
jiapan <jiapan.china@gmail.com>
2023-08-25 16:55:44 +0800

    20230825

---
+++ 1143-longest-common-subsequence/main.go
@@ -0,0 +1,27 @@
+package main
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, n+1)
+	}
+	for i, c1 := range text1 {
+		for j, c2 := range text2 {
+			if c1 == c2 {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+			}
+		}
+	}
+
+	return dp[m][n]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
