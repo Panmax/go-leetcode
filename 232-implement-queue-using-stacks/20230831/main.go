48ec15b9ce06348f1a0fcd908928c07f5657dd54
jiapan <jiapan.china@gmail.com>
2023-08-31 13:46:58 +0800

    20230831

---
+++ 232-implement-queue-using-stacks/20230831/main.go
@@ -0,0 +1,50 @@
+package main
+
+type MyQueue struct {
+	in, out []int
+}
+
+func Constructor() MyQueue {
+	return MyQueue{}
+}
+
+func (this *MyQueue) Push(x int) {
+	this.in = append(this.in, x)
+}
+
+func (this *MyQueue) Pop() int {
+	if len(this.out) <= 0 {
+		this.in2out()
+	}
+	x := this.out[len(this.out)-1]
+	this.out = this.out[:len(this.out)-1]
+	return x
+}
+
+func (this *MyQueue) Peek() int {
+	if len(this.out) <= 0 {
+		this.in2out()
+	}
+	x := this.out[len(this.out)-1]
+	return x
+}
+
+func (this *MyQueue) Empty() bool {
+	return len(this.in) == 0 && len(this.out) == 0
+}
+
+func (this *MyQueue) in2out() {
+	for len(this.in) > 0 {
+		this.out = append(this.out, this.in[len(this.in)-1])
+		this.in = this.in[:len(this.in)-1]
+	}
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
