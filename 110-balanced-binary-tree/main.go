1149e68902d1b01a2296f320e4ae3f63e867ad7a
jiapan <jiapan.china@gmail.com>
2023-09-20 14:08:57 +0800

    20230920

---
+++ 110-balanced-binary-tree/main.go
@@ -0,0 +1,35 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return abc(height(root.Left)-height(root.Right)) <= 1 && isBalanced(root.Left) && isBalanced(root.Right)
+}
+
+func height(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+	return max(height(node.Left), height(node.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func abc(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
---
+++ 137-single-number-ii/review_20230920.go
@@ -0,0 +1,16 @@
+package main
+
+func singleNumber_20230920(nums []int) int {
+	var ans int32
+	for i := 0; i < 32; i++ {
+		var total int
+		for _, num := range nums {
+			total += num >> i & 1
+		}
+		if total%3 == 1 {
+			ans |= 1 << i
+		}
+	}
+
+	return int(ans)
+}
