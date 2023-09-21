07adddd6305880755f802243dc8e640ee101dd89
jiapan <jiapan.china@gmail.com>
2023-09-21 10:15:49 +0800

    20230921

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/review_20230921.go
@@ -0,0 +1,19 @@
+package main
+
+// [ 根节点, [左子树的前序遍历结果], [右子树的前序遍历结果] ]
+// [ [左子树的中序遍历结果], 根节点, [右子树的中序遍历结果] ]
+func buildTree_20230921(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val: preorder[0]}
+	var i int
+	for ; i < len(inorder); i++ {
+		if preorder[0] == inorder[i] {
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:len(inorder[:i])+1], inorder[:i])
+	root.Right = buildTree(preorder[len(inorder[:i])+1:], inorder[i+1:])
+	return root
+}
