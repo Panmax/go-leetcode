1fa7524b30db243eb413c133c80670b190177f40
jiapan <jiapan.china@gmail.com>
2023-09-27 13:18:35 +0800

    20230927

---
+++ 144-binary-tree-preorder-traversal/review_20230927.go
@@ -0,0 +1,16 @@
+package main
+
+func preorderTraversal_20230927(root *TreeNode) []int {
+	var ans []int
+	var preorder func(node *TreeNode)
+	preorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		ans = append(ans, node.Val)
+		preorder(node.Left)
+		preorder(node.Right)
+	}
+	preorder(root)
+	return ans
+}
---
+++ 7-reverse-integer/review_20230927.go
@@ -0,0 +1,16 @@
+package main
+
+import "math"
+
+func reverseInt_20230927(x int) int {
+	var res int
+	for x != 0 {
+		mod := x % 10
+		x /= 10
+		res = res*10 + mod
+		if res > math.MaxInt32 || res < math.MinInt32 {
+			return 0
+		}
+	}
+	return res
+}
