421c514163565eec68b23a039a43091ab2619203
jiapan <jiapan.china@gmail.com>
2023-10-10 10:25:31 +0800

    20231010

---
+++ 1143-longest-common-subsequence/review_20231009.go
@@ -0,0 +1,20 @@
+package main
+
+func longestCommonSubsequence_20231009(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i := range text1 {
+		for j := range text2 {
+			if text1[i] == text2[j] {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+			}
+		}
+	}
+
+	return dp[m][n]
+}
---
+++ 24-swap-nodes-in-pairs/review_202301010.go
@@ -0,0 +1,11 @@
+package main
+
+func swapPairs_20231010(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	next := head.Next
+	head.Next = swapPairs(next.Next)
+	next.Next = head
+	return next
+}
