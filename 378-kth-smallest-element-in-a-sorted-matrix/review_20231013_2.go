a6328525eef2e282cbf3e9999d60ebd01f118f88
jiapan <jiapan.china@gmail.com>
2023-10-13 17:44:48 +0800

    20231013

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/review_20231013_2.go
@@ -0,0 +1,30 @@
+package main
+
+func kthSmallest_20231013_2(matrix [][]int, k int) int {
+	n := len(matrix)
+	left, right := matrix[0][0], matrix[n-1][n-1]
+	for left < right {
+		mid := left + (right-left)/2
+		if isInLeft_20231013_2(matrix, mid, k, n) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func isInLeft_20231013_2(matrix [][]int, mid int, k int, n int) bool {
+	var count int
+	i, j := n-1, 0
+	for i >= 0 && j < n {
+		if matrix[i][j] <= mid {
+			count += i + 1
+			j++
+		} else {
+			i--
+		}
+	}
+
+	return count >= k
+}
