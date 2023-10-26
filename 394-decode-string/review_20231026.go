03fde170916f39fd8b9f2eb948896cda8eadc65c
jiapan <jiapan.china@gmail.com>
2023-10-26 11:42:43 +0800

    20231026

---
+++ 394-decode-string/review_20231026.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func decodeString_20231026(s string) string {
+	n := len(s)
+	var stack []string
+	var ptr int
+	for ptr < n {
+		if s[ptr] >= '0' && s[ptr] <= '9' {
+			d := getDigits(s, &ptr)
+			stack = append(stack, d)
+		} else if (s[ptr] >= 'a' && s[ptr] <= 'z') || (s[ptr] >= 'A' && s[ptr] <= 'Z') || s[ptr] == '[' {
+			stack = append(stack, string(s[ptr]))
+			ptr++
+		} else {
+			ptr++
+			var sub []string
+			for len(stack) > 0 {
+				if stack[len(stack)-1] == "[" {
+					break
+				}
+				sub = append(sub, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = stack[:len(stack)-1]
+			for i := 0; i < len(sub)/2; i++ {
+				sub[i], sub[len(sub)-1-i] = sub[len(sub)-1-i], sub[i]
+			}
+			d, _ := strconv.Atoi(stack[len(stack)-1])
+			stack = stack[:len(stack)-1]
+			repeatStr := strings.Repeat(getString(sub), d)
+			stack = append(stack, repeatStr)
+		}
+	}
+
+	return getString(stack)
+}
