9eb42ec3f8095920f0388f64e49582063a2790c7
jiapan <jiapan.china@gmail.com>
2023-10-26 12:53:17 +0800

    20231026

---
+++ 6-zigzag-conversion/review_20231026.go
@@ -0,0 +1,22 @@
+package mian
+
+func convert_20231026(s string, numRows int) string {
+	if numRows <= 1 {
+		return s
+	}
+	rows := make([]string, numRows)
+	i := 0
+	flag := -1
+	for _, c := range s {
+		rows[i] += string(c)
+		if i == 0 || i == numRows-1 {
+			flag = -flag
+		}
+		i += flag
+	}
+	var res string
+	for _, row := range rows {
+		res += row
+	}
+	return res
+}
