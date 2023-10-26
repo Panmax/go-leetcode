2384adef3d385b1ae719e5fb1582a00ff701518e
jiapan <jiapan.china@gmail.com>
2023-10-26 09:58:00 +0800

    20231025

---
+++ 144-binary-tree-preorder-traversal/review_20231025.go
@@ -0,0 +1,17 @@
+package main
+
+func preorderTraversal_20231025(root *TreeNode) []int {
+	var ans []int
+	var preorder func(node *TreeNode)
+	preorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		ans = append(ans, node.Val)
+		preorder(node.Left)
+		preorder(node.Right)
+	}
+	preorder(root)
+
+	return ans
+}
