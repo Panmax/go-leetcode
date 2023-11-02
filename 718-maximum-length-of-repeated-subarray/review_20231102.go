81bebcdb07126a60918b93f98a276dcdf276790a
jiapan <jiapan.china@gmail.com>
2023-11-02 10:55:49 +0800

    20231102

---
+++ 718-maximum-length-of-repeated-subarray/review_20231102.go
@@ -0,0 +1,23 @@
+package main
+
+func findLength_20231102(nums1 []int, nums2 []int) int {
+	m, n := len(nums1), len(nums2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	var ans int
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = 0
+			}
+			if dp[i][j] > ans {
+				ans = dp[i][j]
+			}
+		}
+	}
+	return ans
+}
