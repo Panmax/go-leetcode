7a83864cf693256581939d56b46e19a7eb48eb39
jiapan <jiapan.china@gmail.com>
2023-11-16 10:49:18 +0800

    20231116

---
+++ 19-remove-nth-node-from-end-of-list/review_20231116.go
@@ -0,0 +1,18 @@
+package main
+
+func removeNthFromEnd_20231116(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{
+		Next: head,
+	}
+	slow := dummy
+	fast := head
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
