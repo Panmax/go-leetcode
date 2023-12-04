05f159b7ccf569cafb205582d6b38fc63f5036a7
jiapan <jiapan.china@gmail.com>
2023-12-04 11:20:24 +0800

    20231204

---
+++ 50-powx-n/review_20231204.go
@@ -0,0 +1,19 @@
+package main
+
+func myPow_20231204(x float64, n int) float64 {
+	if n > 0 {
+		return quickMul_20231204(x, n)
+	}
+	return 1 / quickMul_20231204(x, n)
+}
+
+func quickMul_20231204(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	y := quickMul_20231204(x, n/2)
+	if n%2 == 0 {
+		return y * y
+	}
+	return y * y * x
+}
