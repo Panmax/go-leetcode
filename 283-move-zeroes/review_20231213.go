aa61d0b5282c6fc9131ea17f89d58a506574d395
jiapan <jiapan.china@gmail.com>
2023-12-12 12:51:28 +0800

    20231213

---
+++ 283-move-zeroes/review_20231213.go
@@ -0,0 +1,11 @@
+package main
+
+func moveZeroes_20231213(nums []int) {
+	var j int
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
+		}
+	}
+}
