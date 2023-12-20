46f595e5e5d4e80b85bc08feddf0bc0beb4d8a81
jiapan <jiapan.china@gmail.com>
2023-12-20 10:15:01 +0800

    20231220

---
+++ 45-jump-game-ii/review_20231220.go
@@ -0,0 +1,16 @@
+package main
+
+func jump_20231220(nums []int) int {
+	var step, end, maxPos int
+	for i, num := range nums {
+		if i == len(nums)-1 {
+			break
+		}
+		maxPos = max(maxPos, i+num)
+		if i == end {
+			end = maxPos
+			step++
+		}
+	}
+	return step
+}
