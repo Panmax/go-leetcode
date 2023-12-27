17fca1fd9c497177f40c89b269f910fa26acdea2
jiapan <jiapan.china@gmail.com>
2023-12-27 12:47:11 +0800

    20231227

---
+++ 518-coin-change-ii/review_20231227.go
@@ -0,0 +1,12 @@
+package main
+
+func change_20231227(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1
+	for _, coin := range coins {
+		for i := coin; i <= amount; i++ {
+			dp[i] += dp[i-coin]
+		}
+	}
+	return dp[amount]
+}
