0a126353674ffd0e33d2b05c65f643095a0717af
jiapan <jiapan.china@gmail.com>
2024-01-15 10:19:53 +0800

    20240115

---
+++ 225-implement-stack-using-queues/review_20240115/main.go
@@ -0,0 +1,32 @@
+package main
+
+type MyStack struct {
+	queue1, queue2 []int
+}
+
+func Constructor() MyStack {
+	return MyStack{}
+}
+
+func (this *MyStack) Push(x int) {
+	this.queue2 = append(this.queue2, x)
+	for len(this.queue1) > 0 {
+		this.queue2 = append(this.queue2, this.queue1[0])
+		this.queue1 = this.queue1[1:]
+	}
+	this.queue1, this.queue2 = this.queue2, this.queue1
+}
+
+func (this *MyStack) Pop() int {
+	value := this.queue1[0]
+	this.queue1 = this.queue1[1:]
+	return value
+}
+
+func (this *MyStack) Top() int {
+	return this.queue1[0]
+}
+
+func (this *MyStack) Empty() bool {
+	return len(this.queue1) == 0
+}
