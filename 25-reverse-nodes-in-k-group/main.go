0bc15d5084ea1d09a4dcd50f46f0c516dd430844
jiapan <jiapan.china@gmail.com>
2024-01-24 12:49:06 +0800

    20240124

---
+++ 25-reverse-nodes-in-k-group/main.go
@@ -0,0 +1,41 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	hair := &ListNode{Next: head}
+	pre := hair
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return hair.Next
+			}
+		}
+		next := tail.Next
+		head, tail = reverse(head, tail)
+		pre.Next = head
+		tail.Next = next
+		pre = tail
+		head = tail.Next
+	}
+
+	return hair.Next
+}
+
+func reverse(head *ListNode, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	p := head
+	for prev != tail {
+		next := p.Next
+		p.Next = prev
+		prev = p
+		p = next
+	}
+	return tail, head
+}
