e2ca72c9475703f52e7bd8fced3c2affd9464917
jiapan <jiapan.china@gmail.com>
2024-02-05 13:55:14 +0800

    20240205

---
+++ 1143-longest-common-subsequence/review_20240205.go
@@ -0,0 +1,19 @@
+package main
+
+func longestCommonSubsequence_20240205(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if text1[i] == text2[j] {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+			}
+		}
+	}
+	return dp[m][n]
+}
