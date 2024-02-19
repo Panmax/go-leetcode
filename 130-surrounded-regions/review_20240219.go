39b1708be1008eb5d1a4597da49b802942114ced
jiapan <jiapan.china@gmail.com>
2024-02-19 14:29:00 +0800

    20240219

---
+++ 130-surrounded-regions/review_20240219.go
@@ -0,0 +1,37 @@
+package main
+
+func solve_20240219(board [][]byte) {
+	n := len(board)
+	if n <= 0 {
+		return
+	}
+	m := len(board[0])
+	var dfs func(x, y int)
+	dfs = func(x, y int) {
+		if x < 0 || x >= n || y < 0 || y >= m || board[x][y] != 'O' {
+			return
+		}
+		board[x][y] = 'A'
+		dfs(x+1, y)
+		dfs(x-1, y)
+		dfs(x, y+1)
+		dfs(x, y-1)
+	}
+	for i := 0; i < n; i++ {
+		dfs(i, 0)
+		dfs(i, m-1)
+	}
+	for i := 0; i < m; i++ {
+		dfs(0, i)
+		dfs(n-1, i)
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if board[i][j] == 'A' {
+				board[i][j] = 'O'
+			} else if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			}
+		}
+	}
+}
