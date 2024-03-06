f1cc69ade76cba5a750d5ae8220c71c40b9658cd
jiapan <jiapan.china@gmail.com>
2024-03-06 14:15:48 +0800

    20240306

---
+++ 25-reverse-nodes-in-k-group/review_20240306.go
@@ -0,0 +1,35 @@
+package main
+
+func reverseKGroup_20240306(head *ListNode, k int) *ListNode {
+	hair := &ListNode{Next: head}
+	pre := hair
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return hair.Next
+			}
+		}
+		next := tail.Next
+		head, tail = reverse_20240306(head, tail)
+		pre.Next = head
+		tail.Next = next
+		pre = tail
+		head = next
+	}
+	return hair.Next
+}
+
+func reverse_20240306(head *ListNode, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	p := head
+	for prev != tail {
+		next := p.Next
+		p.Next = prev
+		prev = p
+		p = next
+	}
+	return tail, head
+}
