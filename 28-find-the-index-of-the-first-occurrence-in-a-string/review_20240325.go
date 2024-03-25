210c0cdeb13cb5b23aacb233aea0640d80d5eb1a
jiapan <jiapan.china@gmail.com>
2024-03-25 18:19:01 +0800

    20240325

--- 28-find-the-index-of-the-first-occurrence-in-a-string/review_20240325.go
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/review_20240325.go
@@ -1,12 +1,12 @@
 package main
 
-func strStr_20230817(haystack string, needle string) int {
+func strStr_20240325(haystack string, needle string) int {
 	if len(needle) == 0 {
 		return 0
 	}
 	for i := 0; i < len(haystack); i++ {
 		if i+len(needle) > len(haystack) {
-			break
+			return -1
 		}
 		for j := 0; j < len(needle); j++ {
 			if haystack[i+j] != needle[j] {
