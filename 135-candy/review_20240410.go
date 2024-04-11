8b19e0397c7caa8d679b58e2ef1d043d4b8d43bd
jiapan <jiapan.china@gmail.com>
2024-04-11 14:02:24 +0800

    20240410

---
+++ 135-candy/review_20240410.go
@@ -0,0 +1,24 @@
+package main
+
+func candy_20240410(ratings []int) int {
+	n := len(ratings)
+	left := make([]int, n)
+	var res int
+	for i := range ratings {
+		if i > 0 && ratings[i] > ratings[i-1] {
+			left[i] = left[i-1] + 1
+		} else {
+			left[i] = 1
+		}
+	}
+	var right int
+	for i := n - 1; i >= 0; i-- {
+		if i < n-1 && ratings[i] > ratings[i+1] {
+			right++
+		} else {
+			right = 1
+		}
+		res += max(right, left[i])
+	}
+	return res
+}
