e415492e6b9ebddffa70d70c3afc43ae9e55e460
jiapan <jiapan.china@gmail.com>
2024-05-07 10:58:15 +0800

    20240506

---
+++ 112-path-sum/review_20240506.go
@@ -0,0 +1,11 @@
+package main
+
+func hasPathSum_20240506(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return root.Val == targetSum
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
