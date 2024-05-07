960c105a00fa62d71d432094c2d31dadbf9509dd
jiapan <jiapan.china@gmail.com>
2024-05-07 11:23:36 +0800

    20240507

---
+++ 11-container-with-most-water/review_20240507.go
@@ -0,0 +1,20 @@
+package main
+
+func maxArea_20230507(height []int) int {
+	var max int
+	left, right := 0, len(height)-1
+	for left < right {
+		var area int
+		if height[left] < height[right] {
+			area = height[left] * (right - left)
+			left++
+		} else {
+			area = height[right] * (right - left)
+			right--
+		}
+		if area > max {
+			max = area
+		}
+	}
+	return max
+}
